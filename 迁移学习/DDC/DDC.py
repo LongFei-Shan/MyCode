dac3a3578401647ab790a6b0699473c9acdb4af1
SLF <318432591@qq.com>
2023-11-29 17:07:26 +0800

    添加迁移学习部分代码DDC,DAN

---
+++ 迁移学习/DDC/DDC.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :DDC.py
+# @Time      :2023/11/26 15:54
+# @Author    :LongFei Shan
+import torch
+from torch import nn
+from Pytorch学习.LMMD import LMMD_loss
+from visdom import Visdom
+import numpy as np
+from Pytorch学习.MMD import MMDLoss, MMDLoss_
+
+
+class DDC_Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.layer1 = nn.Sequential(nn.Conv2d(in_channels=1, out_channels=96, kernel_size=(1, 5), stride=(1, 1)),
+                                    nn.ReLU(), nn.MaxPool2d(kernel_size=(1, 3), stride=(1, 2), padding=0)).to(device)
+        self.layer2 = nn.Sequential(nn.Conv2d(in_channels=96, out_channels=256, kernel_size=(1, 3), stride=(1, 1)),
+                                    nn.ReLU(), nn.MaxPool2d(kernel_size=(1, 3), stride=(1, 2), padding=0)).to(device)
+        self.layer3 = nn.Sequential(nn.Conv2d(in_channels=256, out_channels=384, kernel_size=(1, 3), stride=(1, 1)),
+                                    nn.ReLU()).to(device)
+        self.layer4 = nn.Sequential(nn.Conv2d(in_channels=384, out_channels=384, kernel_size=(1, 3), stride=(1, 1)),
+                                    nn.ReLU()).to(device)
+        self.layer5 = nn.Sequential(nn.Conv2d(in_channels=384, out_channels=256, kernel_size=(1, 3), stride=(1, 1)),
+                                    nn.ReLU(), nn.MaxPool2d(kernel_size=(1, 3), stride=(1, 2), padding=0)).to(device)
+        self.layer6 = nn.Sequential(nn.Flatten()).to(device)
+        self.layer7 = nn.Sequential(nn.Linear(in_features=1280, out_features=4096), nn.ReLU(),
+                                    nn.Dropout(p=0.3)).to(device)
+        self.layer8 = nn.Sequential(nn.Linear(in_features=4096, out_features=4096), nn.ReLU(), nn.Dropout(p=0.3)).to(device)
+        self.layer9 = nn.Sequential(nn.Linear(in_features=4096, out_features=10)).to(device)
+
+    def forward(self, inputs):
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        inputs = torch.from_numpy(inputs).type(torch.float32).to(device)
+        x = self.layer1(inputs)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.layer6(x)
+        x = self.layer7(x)
+        upper_output = self.layer8(x)
+        output = self.layer9(upper_output)
+        return output, upper_output
+
+class DDC(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.ddc_model = DDC_Model()
+
+    def forward(self, source_inputs, target_inputs):
+        s_output, s_upper_output = self.ddc_model(source_inputs)
+        t_output, t_upper_output = self.ddc_model(target_inputs)
+        return s_output, t_output, s_upper_output, t_upper_output
+
+
+def train(source_data, target_data, source_label, target_label, source_vec_label, epochs=100, batch_size=50):
+    # TODO: train
+    ddc = DDC()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    loss_func = nn.CrossEntropyLoss().to(device)
+    optimizer = torch.optim.Adam(params=ddc.parameters(), lr=0.001)
+    # 检查每个参数的 requires_grad 属性
+    for name, param in ddc.named_parameters():
+        print(f'Parameter: {name}, Requires Grad: {param.requires_grad}')
+    # 使用MMD损失函数进行训练
+    lmmd_loss = MMDLoss(kernel_type='gaussian', kernel_mul=5.0, kernel_num=20, fix_sigma=None, degree=5)
+    # visdom可视化
+    viz = Visdom(env="main")
+    viz.line([[0, 0, 0]], [0], win="DDC_train_Loss", opts=dict(title="DDC train Loss", legend=["loss", "acc_s", "acc_t"]))
+    # 开始训练
+    for epoch in range(epochs):
+        for i in range(source_data.shape[0]//batch_size):
+            optimizer.zero_grad()
+            # 从target_data中随机挑选batch_size个数据
+            source_data_temp = source_data[i*batch_size:(i+1)*batch_size]
+            source_vec_label_temp = source_vec_label[i*batch_size:(i+1)*batch_size]
+            if i >= target_data.shape[0]//batch_size:
+                j = i % (target_data.shape[0]//batch_size)
+            else:
+                j = i
+            target_data_temp = target_data[j * batch_size:(j + 1) * batch_size]
+            # 训练
+            source_outputs, target_outputs, s_x, t_x = ddc(source_data_temp, target_data_temp)
+            # 计算损失
+            loss = lmmd_loss(s_x, t_x) + loss_func(source_outputs, torch.from_numpy(source_vec_label_temp).to(device))
+            loss.backward()
+            optimizer.step()
+        source_outputs, target_outputs, _, _, _, _ = ddc(source_data, target_data)
+        # 计算准确率，源域
+        source_outputs = source_outputs.cpu().detach().numpy()
+        source_outputs = np.argmax(source_outputs, axis=1)
+        acc_s = np.mean(source_outputs == source_label)
+        # 计算准确率，目标域
+        target_outputs = target_outputs.cpu().detach().numpy()
+        target_outputs = np.argmax(target_outputs, axis=1)
+        acc_t = np.mean(target_outputs == target_label)
+        viz.line([[loss.item(), acc_s, acc_t]], [epoch], update="append", opts=dict(title="DDC train Loss", legend=["loss", "acc_s", "acc_t"]), win="DDC_train_Loss")
+
+        print(f"epoch:{epoch}, loss:{loss.item()}, acc_s:{acc_s}, acc_t:{acc_t}")
+
---
+++ 迁移学习/DDC/MMD.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :MMD.py
+# @Time      :2023/11/28 10:41
+# @Author    :LongFei Shan
+import torch
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+
+# 定义MMD损失函数
+class MMDLoss_(nn.Module):
+    # 网上嫖的代码
+    def __init__(self, kernel_type='gaussian', kernel_mul=8.0, kernel_num=5):
+        super(MMDLoss_, self).__init__()
+        self.kernel_type = kernel_type
+        self.kernel_mul = kernel_mul
+        self.kernel_num = kernel_num
+        self.fix_sigma = None
+        self.use_cuda = torch.cuda.is_available()
+
+    def gaussian_kernel(self, source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
+        n_samples = int(source.size()[0]) + int(target.size()[0])
+        total = torch.cat([source, target], dim=0)
+
+        total0 = total.unsqueeze(0).expand(int(total.size(0)), int(total.size(0)), int(total.size(1)))
+        total1 = total.unsqueeze(1).expand(int(total.size(0)), int(total.size(0)), int(total.size(1)))
+
+        L2_distance = ((total0-total1)**2).sum(2)
+        if fix_sigma:
+            bandwidth = fix_sigma
+        else:
+            bandwidth = torch.sum(L2_distance.detach()) / (n_samples**2-n_samples)
+        bandwidth /= kernel_mul ** (kernel_num // 2)
+        bandwidth_list = [bandwidth * (kernel_mul**i) for i in range(kernel_num)]
+
+        kernel_val = [torch.exp(-L2_distance / bandwidth_temp) for bandwidth_temp in bandwidth_list]
+        return sum(kernel_val)
+
+    def forward(self, source, target):
+        if self.use_cuda:
+            source = source.cuda()
+            target = target.cuda()
+
+        batch_size = int(source.size()[0])
+
+        kernels = self.gaussian_kernel(source, target, kernel_mul=self.kernel_mul, kernel_num=self.kernel_num, fix_sigma=self.fix_sigma)
+
+        XX = torch.mean(kernels[:batch_size, :batch_size])
+        YY = torch.mean(kernels[batch_size:, batch_size:])
+        XY = torch.mean(kernels[:batch_size, batch_size:])
+        YX = torch.mean(kernels[batch_size:, :batch_size])
+
+        loss = XX + YY - XY - YX
+        loss = torch.clamp(loss, min=0.0)  # 确保损失为非负值
+
+        return loss
+
+
+class MMDLoss(nn.Module):
+    # 重新写的代码
+    def __init__(self, kernel_type='gaussian', kernel_mul=8.0, kernel_num=5, fix_sigma=None, degree=3):
+        super(MMDLoss, self).__init__()
+        self.kernel_type = kernel_type
+        self.kernel_mul = kernel_mul
+        self.kernel_num = kernel_num
+        self.fix_sigma = fix_sigma
+        self.degree = degree
+        self.use_cuda = torch.cuda.is_available()
+
+    def gaussian_kernel(self, X, Y, sigma):
+        """
+        计算高斯核函数
+        X: Tensor，形状为 (batch_size, feature_dim)
+        Y: Tensor，形状为 (batch_size, feature_dim)
+        sigma: 高斯核函数的标准差
+        返回形状为 (batch_size, batch_size) 的核矩阵
+        """
+        M = X.size(0)
+        N = Y.size(0)
+        X = X.unsqueeze(1).expand(-1, N, -1)
+        Y = Y.unsqueeze(0).expand(M, -1, -1)
+        L2_distance = ((X - Y) ** 2).sum(2)
+        n_samples = int(X.size()[0])
+        if sigma is None:
+            sigma = torch.sum(L2_distance.detach()) / (n_samples ** 2 - n_samples)
+        sigma /= self.kernel_mul ** (self.kernel_num // 2)
+        sigma_list = [sigma * (self.kernel_mul ** i) for i in range(self.kernel_num)]
+        kernel_val = [torch.exp(-L2_distance / (sigma_temp + 1e-5)) for sigma_temp in sigma_list]  # 防止sigma为0 + 1e-5
+        kernel_matrix = sum(kernel_val)
+        return kernel_matrix
+
+
+    def linear_kernel(self, X, Y):
+        """
+        计算线性核函数
+        X: Tensor，形状为 (batch_size, feature_dim)
+        Y: Tensor，形状为 (batch_size, feature_dim)
+        返回形状为 (batch_size, batch_size) 的核矩阵
+        """
+        return torch.matmul(X, Y.t())
+
+
+    def cosine_kernel(self, X, Y):
+        """
+        计算余弦核函数
+        X: Tensor，形状为 (batch_size, feature_dim)
+        Y: Tensor，形状为 (batch_size, feature_dim)
+        返回形状为 (batch_size, batch_size) 的核矩阵
+        """
+        X_norm = F.normalize(X, dim=1)
+        Y_norm = F.normalize(Y, dim=1)
+        return torch.matmul(X_norm, Y_norm.t())
+
+
+    def polynomial_kernel(self, X, Y, degree):
+        """
+        计算多项式核函数
+        X: Tensor，形状为 (batch_size, feature_dim)
+        Y: Tensor，形状为 (batch_size, feature_dim)
+        degree: 多项式核函数的次数
+        返回形状为 (batch_size, batch_size) 的核矩阵
+        """
+        return (torch.matmul(X, Y.t()) + 1) ** degree
+
+
+    def forward(self, X, Y):
+        """
+        计算最大均值差异（MMD）损失函数
+        X: Tensor，形状为 (batch_size, feature_dim)
+        Y: Tensor，形状为 (batch_size, feature_dim)
+        kernel_type: 核函数类型，可选值为 'gaussian', 'linear', 'cosine', 'polynomial'
+        kernel_param: 核函数的参数，如高斯核函数的标准差、多项式核函数的次数等
+        返回 MMD 损失
+        """
+        batch_size = X.size(0)
+        K_xx, K_yy, K_xy, K_yx = 0, 0, 0, 0
+        if self.kernel_type == 'gaussian':
+            K_xx = self.gaussian_kernel(X, X, self.fix_sigma)
+            K_yy = self.gaussian_kernel(Y, Y, self.fix_sigma)
+            K_xy = self.gaussian_kernel(X, Y, self.fix_sigma)
+            K_yx = self.gaussian_kernel(Y, X, self.fix_sigma)
+        elif self.kernel_type == 'linear':
+            K_xx = self.linear_kernel(X, X)
+            K_yy = self.linear_kernel(Y, Y)
+            K_xy = self.linear_kernel(X, Y)
+            K_yx = self.linear_kernel(Y, X)
+        elif self.kernel_type == 'cosine':
+            K_xx = self.cosine_kernel(X, X)
+            K_yy = self.cosine_kernel(Y, Y)
+            K_xy = self.cosine_kernel(X, Y)
+            K_yx = self.cosine_kernel(Y, X)
+        elif self.kernel_type == 'poly':
+            K_xx = self.polynomial_kernel(X, X, self.degree)
+            K_yy = self.polynomial_kernel(Y, Y, self.degree)
+            K_xy = self.polynomial_kernel(X, Y, self.degree)
+            K_yx = self.polynomial_kernel(Y, X, self.degree)
+
+        loss = torch.mean(K_xx) + torch.mean(K_yy) - torch.mean(K_xy) - torch.mean(K_yx)
+        loss = torch.clamp(loss, min=0.0)
+
+        return loss
